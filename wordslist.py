68769e0356170e68c87c71266438f02c23480a7e
Abanoub Milad <abanoub.miladmaher@gmail.com>
2026-09-19 05:06:30 +0300

    Add files via upload

---
+++ wordslist.py
@@ -0,0 +1,9 @@
+# Words for the hangman game
+words = (
+    "kratos", "doomguy", "geralt", "dante",
+    "heihachi", "ryu", "scorpion", "subzero",
+    "solidsnake", "bigboss", "snake","raiden",
+    "wukong", "vergil","ghost","titus" ,
+    "calgar", "jinsakai", "joel", "arthur",
+    "trevor", "shepard" , "bigsmoke","armstrong",
+)
